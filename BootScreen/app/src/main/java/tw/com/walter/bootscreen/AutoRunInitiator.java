308e3cf1265d9b300ccac4b61b9d44475fe2494c
Walter-Ke <herdor@gmail.com>
2017-01-08 18:07:07 +0800

    bootwelcome

--- BootScreen/app/src/main/java/tw/com/walter/bootscreen/AutoRunInitiator.java
+++ BootScreen/app/src/main/java/tw/com/walter/bootscreen/AutoRunInitiator.java
@@ -3,6 +3,7 @@ package tw.com.walter.bootscreen;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
 
 public class AutoRunInitiator extends BroadcastReceiver
 {
@@ -15,5 +16,12 @@ public class AutoRunInitiator extends BroadcastReceiver
     {
         
        /*TODO: add Toast and start GAD01 Activity */
+        Toast.makeText(context,"BOOT",Toast.LENGTH_LONG).show();
+
+        Intent intent1 = new Intent(context, MainActivity.class);
+        intent1.addFlags(intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intent1);
+
+
     }
 }
--- BootScreen/app/src/main/java/tw/com/walter/bootscreen/MediaMountInitiator.java
+++ BootScreen/app/src/main/java/tw/com/walter/bootscreen/MediaMountInitiator.java
@@ -5,6 +5,7 @@ package tw.com.walter.bootscreen;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
 
 public class MediaMountInitiator extends BroadcastReceiver
 {
@@ -17,5 +18,6 @@ public class MediaMountInitiator extends BroadcastReceiver
     public void onReceive(Context context, Intent intent)
     {
         /* TODO: add Toast when got SD Card*/
+        Toast.makeText(context,"SD card is mounted", Toast.LENGTH_LONG).show();
     }
 }
